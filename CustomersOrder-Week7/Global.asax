83bfb1c818cc53c439f20d3444d1c75fb78b1a09
Deep Patel <patel795@sheridancollege.ca>
2018-10-17 14:39:37 -0400

    Add project files.

---
+++ CustomersOrder-Week7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomersOrder_Week7.MvcApplication" Language="C#" %>
---
+++ CustomersOrder-Week7/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomersOrder_Week7.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
